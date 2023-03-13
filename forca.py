11171d0d24751a353ec3ede75c0aa0e62c3e0819
Alex Torres <alextorres7736@gmail.com>
2023-03-13 16:28:30 -0300

    Acrescentado desenho a cada erro.

--- forca.py
+++ forca.py
@@ -1,41 +1,58 @@
 import random
 
-
 def jogar():
 
-    imprime_mensagem_abertura()
-    palavra_secreta = carrega_palavra_secreta()
+    jogar_novamente = True
 
-    letras_acertadas = ["_" for letra in palavra_secreta]
-    erros = 0
-    print(letras_acertadas)
+    while jogar_novamente == True:
+        imprime_mensagem_abertura()
+        palavra_secreta = carrega_palavra_secreta()
 
-    enforcou = False
-    acertou = False
-
-    while(not enforcou and not acertou):
-
-        chute = pede_chute()
-
-        if(chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1
-        else:
-            erros += 1
-
-        enforcou = erros == 6
-        acertou = "_" not in letras_acertadas
+        letras_acertadas = ["_" for letra in palavra_secreta]
+        erros = 0
         print(letras_acertadas)
 
-    if(acertou):
-        print("Voce ganhou!!")
-    else:
-        print("Vce perdeu!!")
+        enforcou = False
+        acertou = False
 
-    print("FIM DO JOGO!")
+        while(not enforcou and not acertou):
+
+            chute = pede_chute()
+
+            if(chute in palavra_secreta):
+                index = 0
+                for letra in palavra_secreta:
+                    if(chute == letra):
+                        letras_acertadas[index] = letra
+                    index += 1
+            else:
+                erros += 1
+                desenha_forca(erros)
+
+            enforcou = erros == 7
+            acertou = "_" not in letras_acertadas
+            print(letras_acertadas)
+
+        if(acertou):
+            print("Voce ganhou!!")
+        else:
+            print("Puxa, você foi enforcado!")
+            print("A palavra era {}".format(palavra_secreta))
+        while True:
+            jogar_novamente = input("Deseja jogar novamente? (S/N) ").lower()
+
+            if jogar_novamente == "n":
+                jogar_novamente = False
+                print("FIM DO JOGO!")
+                break
+
+            elif jogar_novamente == "s":
+                print("Vamos jogar novamente!")
+                jogar_novamente = True
+                break
+            else:
+                print("Opção inválida, digite S ou N!")
+
 
 def imprime_mensagem_abertura():
     print("\n***************************")
@@ -58,7 +75,58 @@ def carrega_palavra_secreta():
 def pede_chute():
     chute = input("Qual letra? ")
     chute = chute.strip().upper()
+    chute = chute.replace("C", "Ç")
     return chute
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 
 if(__name__ =="__main__"):
